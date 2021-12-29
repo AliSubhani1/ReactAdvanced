1421752e2956b81bee89144d382a00423321cdfc
Ali Subhani <ali.subhani@tintash.com>
2021-12-29 18:02:43 +0500

    practiced set state hook

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  let [value, setValue] = useState(0);
+  const complexIncrease = () => {
+    setTimeout(() => {
+      setValue((value) => value + 1);
+    }, 2000);
+  };
+  return (
+    <>
+      <section style={{ margin: "4rem 0" }}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          decrease
+        </button>
+        <button className="btn" onClick={() => setValue((value = 0))}>
+          reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          increase
+        </button>
+      </section>
+      <section>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          complex increase
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
